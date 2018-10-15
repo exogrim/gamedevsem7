eeed456cf18e7b9d36e024431acabe7dfcee14b4
Heri Soeparno <herisoeparno@gmail.com>
2018-10-15 16:40:39 +0700

    update room layout generation

--- Assets/Scripts/RoomDetails.cs
+++ Assets/Scripts/RoomDetails.cs
@@ -24,24 +24,91 @@ public class RoomDetails : MonoBehaviour {
 
 	void GenerateRoom()
 	{
+		if (roomType.Equals (RoomType.Entry)) {
+			GenerateLeft ();
+			GenerateRight ();
+			GenerateTop ();
+			GenerateBottom ();
+		}
+
+	}
+
+	void GenerateLeft()
+	{
 		Vector3 newpos;
 		RoomDetails newRoomDetails;
-		if (roomType.Equals (RoomType.Entry)) {
-			
-			doorController = Left.GetComponent<DoorController> ();
+
+		doorController = Left.GetComponent<DoorController> ();
+		if (doorController.isRoomSet ()) {
+			newpos = transform.position;
+			newpos.x -= 30;
+			GameObject newRoom = GameObject.Instantiate (roomTemplate.leftRooms [0]);
+			newRoom.transform.position = newpos;
+			doorController.setRoom (newRoom);
+
+			newRoomDetails = newRoom.GetComponent<RoomDetails> ();
+			doorController = newRoomDetails.Right.GetComponent<DoorController> ();
+			doorController.setRoom (this.gameObject);
+		}
+	}
+
+	void GenerateRight()
+	{
+		Vector3 newpos;
+		RoomDetails newRoomDetails;
+
+		doorController = Right.GetComponent<DoorController> ();
+		if (doorController.isRoomSet ()) {
+			newpos = transform.position;
+			newpos.x += 30;
+			GameObject newRoom = GameObject.Instantiate (roomTemplate.rightRooms [0]);
+			newRoom.transform.position = newpos;
+			doorController.setRoom (newRoom);
+
+			newRoomDetails = newRoom.GetComponent<RoomDetails> ();
+			doorController = newRoomDetails.Left.GetComponent<DoorController> ();
+			doorController.setRoom (this.gameObject);
+		}
+	}
+
+	void GenerateTop()
+	{
+			Vector3 newpos;
+			RoomDetails newRoomDetails;
+
+			doorController = Top.GetComponent<DoorController> ();
 			if (doorController.isRoomSet ()) {
 				newpos = transform.position;
-				newpos.x -= 30;
-				GameObject newRoom = GameObject.Instantiate (roomTemplate.leftRooms [0]);
+				newpos.y += 30;
+				GameObject newRoom = GameObject.Instantiate (roomTemplate.topRooms [0]);
 				newRoom.transform.position = newpos;
 				doorController.setRoom (newRoom);
 
 				newRoomDetails = newRoom.GetComponent<RoomDetails> ();
-				doorController = newRoomDetails.Right.GetComponent<DoorController> ();
+				doorController = newRoomDetails.Bottom.GetComponent<DoorController> ();
 				doorController.setRoom (this.gameObject);
 			}
+		
+	}
 
+	void GenerateBottom()
+	{
+		Vector3 newpos;
+		RoomDetails newRoomDetails;
+
+		doorController = Bottom.GetComponent<DoorController> ();
+		if (doorController.isRoomSet ()) {
+			newpos = transform.position;
+			newpos.y -= 30;
+			GameObject newRoom = GameObject.Instantiate (roomTemplate.bottomRooms [0]);
+			newRoom.transform.position = newpos;
+			doorController.setRoom (newRoom);
+
+			newRoomDetails = newRoom.GetComponent<RoomDetails> ();
+			doorController = newRoomDetails.Top.GetComponent<DoorController> ();
+			doorController.setRoom (this.gameObject);
 		}
+
 	}
 	
 	// Update is called once per frame
